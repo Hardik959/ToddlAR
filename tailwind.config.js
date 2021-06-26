f1ee420a423559fe838b0abf00fdf16b00510762
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-06-26 14:54:40 +0530

    navbar design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nav: "#EFE2BA",
+        navItems: "#4056A1",
+        alphabetsBg: "#F76C6C",
+      },
+      fontFamily: {
+        navFont: ['"Patrick Hand"'],
+      },
+    },
   },
   variants: {
     extend: {},
