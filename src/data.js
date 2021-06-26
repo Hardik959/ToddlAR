dae913dc32d9d8ea2a630973c13c5e23cf34cf94
Hardik Srivastava <hardiksrivastava959@gmail.com>
2021-06-27 03:28:53 +0530

    add data

---
+++ src/data.js
@@ -0,0 +1,495 @@
+const alphabets = [
+    {
+      ar: ( 'a'
+      //   <iframe
+      //     id='1d5c1d8c-959c-4312-a1dd-5c7cca66bfb9'
+      //     src='https://www.vectary.com/viewer/v1/?model=1d5c1d8c-959c-4312-a1dd-5c7cca66bfb9&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/a.png",
+    },
+    {
+      ar: ('b'
+      //   <iframe
+      //     id='735527fb-b3ba-4232-8b90-25aaf441510a'
+      //     src='https://www.vectary.com/viewer/v1/?model=735527fb-b3ba-4232-8b90-25aaf441510a&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/b.png",
+    },
+    {
+      ar: ('c'
+      //   <iframe
+      //     id='e072cead-03c9-418a-bf2d-fe289a8fcdb4'
+      //     src='https://www.vectary.com/viewer/v1/?model=e072cead-03c9-418a-bf2d-fe289a8fcdb4&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+  
+      qr: "../images/alphabets/c.png",
+    },
+    {
+      ar: ('d'
+      //   <iframe
+      //     id='60fcb9e2-c764-432d-9290-fec6ec3d9b06'
+      //     src='https://www.vectary.com/viewer/v1/?model=60fcb9e2-c764-432d-9290-fec6ec3d9b06&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/d.png",
+    },
+    {
+      ar: ('e'
+      //   <iframe
+      //     id='84c5ad54-82b1-4633-86b2-1af62fc5ec03'
+      //     src='https://www.vectary.com/viewer/v1/?model=84c5ad54-82b1-4633-86b2-1af62fc5ec03&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/e.png",
+    },
+    {
+      ar: ('f'
+      //   <iframe
+      //     id='01e6c7ae-9f08-4ba4-8f17-aa0101dc1c6b'
+      //     src='https://www.vectary.com/viewer/v1/?model=01e6c7ae-9f08-4ba4-8f17-aa0101dc1c6b&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/f.png",
+    },
+    {
+      ar: ('g'
+      //   <iframe
+      //     id='44d6a329-463a-486f-b518-22cf5a494491'
+      //     src='https://www.vectary.com/viewer/v1/?model=44d6a329-463a-486f-b518-22cf5a494491&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/g.png",
+    },
+    {
+      ar: ('h'
+      //   <iframe
+      //     id='3d7f150e-c94b-416f-985b-6c7d7751de79'
+      //     src='https://www.vectary.com/viewer/v1/?model=3d7f150e-c94b-416f-985b-6c7d7751de79&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/h.png",
+    },
+    {
+      ar: ('i'
+      //   <iframe
+      //     id='6b664480-d065-4f0b-a9a7-c69e775bcfbe'
+      //     src='https://www.vectary.com/viewer/v1/?model=6b664480-d065-4f0b-a9a7-c69e775bcfbe&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/i.png",
+    },
+    {
+      ar: ('j'
+      //   <iframe
+      //     id='ab259749-33a9-4acc-83ad-5ecab0293bc2'
+      //     src='https://www.vectary.com/viewer/v1/?model=ab259749-33a9-4acc-83ad-5ecab0293bc2&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/j.png",
+    },
+    {
+      ar: ('k'
+      //   <iframe
+      //     id='a429695c-575d-4149-84b8-b967aa581546'
+      //     src='https://www.vectary.com/viewer/v1/?model=a429695c-575d-4149-84b8-b967aa581546&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/k.png",
+    },
+    {
+      ar: ('l'
+      //   <iframe
+      //     id='451d2b4e-0e04-4e50-a46c-216963a8d14d'
+      //     src='https://www.vectary.com/viewer/v1/?model=451d2b4e-0e04-4e50-a46c-216963a8d14d&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/l.png",
+    },
+    {
+      ar: ('m'
+      //   <iframe
+      //     id='61856722-6321-4cce-acbc-8182ce6e11a9'
+      //     src='https://www.vectary.com/viewer/v1/?model=61856722-6321-4cce-acbc-8182ce6e11a9&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/m.png",
+    },
+    {
+      ar: ('n'
+      //   <iframe
+      //     id='c4f129e1-bdd7-4622-8f5c-f90818e279c3'
+      //     src='https://www.vectary.com/viewer/v1/?model=c4f129e1-bdd7-4622-8f5c-f90818e279c3&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/n.png",
+    },
+    {
+      ar: ('o'
+      //   <iframe
+      //     id='4bbcb8b3-44a9-45de-9281-38958cc7a587'
+      //     src='https://www.vectary.com/viewer/v1/?model=4bbcb8b3-44a9-45de-9281-38958cc7a587&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/o.png",
+    },
+    {
+      ar: ('p'
+      //   <iframe
+      //     id='4f94d045-79a4-430c-9d56-45d43be7b275'
+      //     src='https://www.vectary.com/viewer/v1/?model=4f94d045-79a4-430c-9d56-45d43be7b275&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/p.png",
+    },
+    {
+      ar: ('q'
+      //   <iframe
+      //     id='46979fb0-67a9-4361-9dd6-cf23c6ed1612'
+      //     src='https://www.vectary.com/viewer/v1/?model=46979fb0-67a9-4361-9dd6-cf23c6ed1612&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/q.png",
+    },
+    {
+      ar: ('r'
+      //   <iframe
+      //     id='8315d23e-ccee-4e75-87db-3f66272043c0'
+      //     src='https://www.vectary.com/viewer/v1/?model=8315d23e-ccee-4e75-87db-3f66272043c0&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/r.png",
+    },
+    {
+      ar: ('s'
+      //   <iframe
+      //     id='840257ba-2f9f-40d7-b9cf-8e2712b39af5'
+      //     src='https://www.vectary.com/viewer/v1/?model=840257ba-2f9f-40d7-b9cf-8e2712b39af5&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/s.png",
+    },
+    {
+      ar: ('t'
+      //   <iframe
+      //     id='ab286a75-3131-4249-bf2d-65e98ee13848'
+      //     src='https://www.vectary.com/viewer/v1/?model=ab286a75-3131-4249-bf2d-65e98ee13848&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/t.png",
+    },
+    {
+      ar: ('u'
+      //   <iframe
+      //     id='70fb60b6-a8cf-4c41-8b1a-8a4a59ee7a24'
+      //     src='https://www.vectary.com/viewer/v1/?model=70fb60b6-a8cf-4c41-8b1a-8a4a59ee7a24&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/u.png",
+    },
+    {
+      ar: ('v'
+      //   <iframe
+      //     id='30844252-cac5-4de3-8f99-a4c39b620825'
+      //     src='https://www.vectary.com/viewer/v1/?model=30844252-cac5-4de3-8f99-a4c39b620825&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/v.png",
+    },
+    {
+      ar: ('w'
+      //   <iframe
+      //     id='93c96a9c-d86e-43ae-872e-fe6f8125becf'
+      //     src='https://www.vectary.com/viewer/v1/?model=93c96a9c-d86e-43ae-872e-fe6f8125becf&env=studio3'
+      //     frameborder='0'
+      //     width='100%'></iframe>
+      ),
+      qr: "../images/alphabets/w.png",
+    },
+    {
+      ar: ('x'
+      //   <iframe
+      //     id='dd42d8ca-d689-4195-aae1-b74b4a93be50'
+      //     src='https://www.vectary.com/viewer/v1/?model=dd42d8ca-d689-4195-aae1-b74b4a93be50&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/x.png",
+    },
+    {
+      ar: ('y'
+      //   <iframe
+      //     id='975b4598-a9c6-4e8e-967b-e92a6aa2f379'
+      //     src='https://www.vectary.com/viewer/v1/?model=975b4598-a9c6-4e8e-967b-e92a6aa2f379&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/y.png",
+    },
+    {
+      ar: ('z'
+      //   <iframe
+      //     id='a06ae09d-e630-4a14-b8ad-cd546425ae40'
+      //     src='https://www.vectary.com/viewer/v1/?model=a06ae09d-e630-4a14-b8ad-cd546425ae40&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+      qr: "../images/alphabets/z.png",
+    },
+  ];
+  
+  const numbers = [
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='24bc4c2d-d34d-46a8-9459-8f3d5348237f'
+      //     src='https://www.vectary.com/viewer/v1/?model=24bc4c2d-d34d-46a8-9459-8f3d5348237f&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='f1921789-3fef-458f-9903-ceba909cd860'
+      //     src='https://www.vectary.com/viewer/v1/?model=f1921789-3fef-458f-9903-ceba909cd860&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='8a801e99-1b28-439b-bb05-7ba5d0ef34cd'
+      //     src='https://www.vectary.com/viewer/v1/?model=8a801e99-1b28-439b-bb05-7ba5d0ef34cd&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='d0d2145d-0fd6-4fcb-b59c-e2f4c4dce065'
+      //     src='https://www.vectary.com/viewer/v1/?model=d0d2145d-0fd6-4fcb-b59c-e2f4c4dce065&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='655b3c6a-0795-40c5-a7b5-6b44eed8f6a3'
+      //     src='https://www.vectary.com/viewer/v1/?model=655b3c6a-0795-40c5-a7b5-6b44eed8f6a3&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='22f2e8a0-0cde-4898-9dd6-7ea3679bc4e7'
+      //     src='https://www.vectary.com/viewer/v1/?model=22f2e8a0-0cde-4898-9dd6-7ea3679bc4e7&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='b3ba1be8-35a5-481f-8788-7f4f4125f957'
+      //     src='https://www.vectary.com/viewer/v1/?model=b3ba1be8-35a5-481f-8788-7f4f4125f957&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='c227b8be-6ba2-4181-8faf-b2dc70724fb6'
+      //     src='https://www.vectary.com/viewer/v1/?model=c227b8be-6ba2-4181-8faf-b2dc70724fb6&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='38f79ff1-2225-45dc-bbe9-d9ed037ce9a2'
+      //     src='https://www.vectary.com/viewer/v1/?model=38f79ff1-2225-45dc-bbe9-d9ed037ce9a2&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='7b40ff2b-ca1e-42b0-9c73-31bf9ccb636a'
+      //     src='https://www.vectary.com/viewer/v1/?model=7b40ff2b-ca1e-42b0-9c73-31bf9ccb636a&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+  ];
+  
+  const shapes = [
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='f855beaf-8301-40d1-ba73-0d76572edfe7'
+      //     src='https://www.vectary.com/viewer/v1/?model=f855beaf-8301-40d1-ba73-0d76572edfe7&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='fefaa7c5-f4be-4382-a541-6924007d68b7'
+      //     src='https://www.vectary.com/viewer/v1/?model=fefaa7c5-f4be-4382-a541-6924007d68b7&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='87245bf0-1088-4daf-8b0f-c522031fcafc'
+      //     src='https://www.vectary.com/viewer/v1/?model=87245bf0-1088-4daf-8b0f-c522031fcafc&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='75c1316c-df65-4baf-ba19-a2725298fd4d'
+      //     src='https://www.vectary.com/viewer/v1/?model=75c1316c-df65-4baf-ba19-a2725298fd4d&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+    {
+      ar: ('ar'
+      //   <iframe
+      //     id='e8483b85-14cb-4817-a24c-65bbd8ce9bab'
+      //     src='https://www.vectary.com/viewer/v1/?model=e8483b85-14cb-4817-a24c-65bbd8ce9bab&env=studio3'
+      //     frameborder='0'
+      //     width='100%'
+      //     height='480'></iframe>
+      ),
+    },
+  ];
+  
+  const animals = [
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=02e67f39-8e54-4452-b42b-eed8752606a9' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=818c8316-8433-470f-978b-966a8679ca15' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=75936428-6bc3-4655-aa2f-ba9017b70650' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=45b1cc8b-3f56-4c54-83dd-6e4fa17e2c38' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=e0890834-ac73-4709-86b4-cb64aaf1fbdf' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=0b336ef1-b9fe-4373-9f7a-73620934cec6' />
+      ),
+    },
+  ];
+  
+  const birds = [
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=cf601d63-87be-4477-a083-b3541b623af7' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=7a688a5e-ba26-4d86-8713-df67a6946b7b' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=a3176292-623c-4f21-927b-b7a82927abc2' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=a4cbb7f1-a8a0-41c3-8399-fe99ebf5ddfa' />
+      ),
+    },
+    {
+      ar: (
+        <iframe src='https://console.echoAR.xyz/webar?key=gentle-breeze-9075&entry=9c014504-b879-439f-9cce-de3b23f69480' />
+      ),
+    },
+  ];
+  // 
+  export { alphabets, numbers, shapes, animals, birds };
+  
